c3a4c84e94ef171e55d0a34a8051bd76cc53cc79
iarielduarte <iariel.javauser@gmail.com>
2012-08-25 21:50:12 +0100

    Primer proyecto con Servlet

---
+++ TestWeb/src/aplic/HelloServlet.java
@@ -0,0 +1,43 @@
+package aplic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html"); ////Se define el tipo de respuesta 
+		PrintWriter salida = response.getWriter();////Capture la respuesta 
+		salida.println("<h1>Hello Servlet</h1>");///Y se escribe el mensaje
+		salida.close();///Se cierra la respuesta
+	}
+
+}
